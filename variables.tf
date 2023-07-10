822092053868f68da492964076194c70a2233154
Swaroop Shankar D H <swaroop.shankardh@dell.com>
2023-07-10 12:40:35 -0400

    changes to remove external data sources

--- variables.tf
+++ variables.tf
@@ -233,3 +233,15 @@ variable "skip_metadata_api_check" {
 variable "internal_sg_id" {
   description = "Security group ID to be attached to the internal network interfaces"
 }
+
+variable "root_passphrase" {
+  type        = string
+  default     = null
+  description = "hashed root passphrase to create the onefs cluster"
+}
+
+variable "admin_passphrase" {
+  type        = string
+  default     = null
+  description = "hashed admin passphrase to create the onefs cluster"
+}
