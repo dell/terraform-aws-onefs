ce6e68acb73c02e6836d6c3d7d14d00c5045edf8
Tom Swanson <Tom.Swanson@dell.com>
2023-10-30 19:33:51 +0000

    [PSCALE-199095] - Tags name name and name name of onefs_placement_group were at odds.

--- modules/base/onefsbase.tf
+++ modules/base/onefsbase.tf
@@ -212,9 +212,6 @@ resource "aws_placement_group" "onefs_placement_group" {
   strategy = local.placement_group_strategy
   tags = merge(
     local.resource_tags,
-    {
-      Name = "${var.id}-placement-group"
-    }
   )
 
   partition_count = local.partition_count
